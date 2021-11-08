2c84fbde0454c98ef60dd1cc57439e5b560c490a
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-11-08 11:20:11 +0100

    nice

---
+++ Car.php
@@ -0,0 +1,84 @@
+<?php
+class Car extends Vehicle implements LightableInterface
+{
+    public const ALLOWED_ENERGIES = [
+        'Fuel',
+        'Electric',
+    ];
+    protected string $energy;
+    protected int $energyLevel = 100;
+    protected bool $isEngineOn = false;
+    private bool $hasParkBrake = true;
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
+        $this->nbWheels = 4;
+        $this->currentSpeed = 0;
+    }
+
+    public function switchOn(): bool
+    {
+        return true;
+    }
+
+    public function switchOff(): bool
+    {
+        return false;
+    }
+
+    public function start(): string
+    {
+        if ($this->getHasParkBrake()) {
+            throw new Exception("This car is parked");
+        }
+        if ($this->isEngineOn) {
+            return 'Engine is already On';
+        } else {
+            $this->isEngineOn = true;
+            return 'Engine start';
+        }
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy($energy): void
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel($energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+
+    public function getIsEngineOn(): bool
+    {
+        return $this->isEngineOn;
+    }
+
+    public function setIsEngineOn($isEngineOn): void
+    {
+        $this->isEngineOn = $isEngineOn;
+    }
+
+    public function setHasParkBrake(bool $hasParkBrake)
+    {
+        $this->hasParkBrake = $hasParkBrake;
+    }
+
+    public function getHasParkBrake(): bool
+    {
+        return $this->hasParkBrake;
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'Bicycle.php';
+require_once 'Skateboard.php';
+
+$bicycle = new Bicycle('red', 2);
+
